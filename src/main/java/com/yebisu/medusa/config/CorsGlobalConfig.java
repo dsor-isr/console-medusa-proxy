663f66a448ebc9ce06cef79d632f02dfbba800a2
pbayona <eddybayonne@gmail.com>
2021-01-07 22:02:06 +0000

    added CORS in order to allow origins

---
+++ src/main/java/com/yebisu/medusa/config/CorsGlobalConfig.java
@@ -0,0 +1,17 @@
+package com.yebisu.medusa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsGlobalConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET, POST");
+    }
+}
