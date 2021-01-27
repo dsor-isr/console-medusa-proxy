8c5f11295cf45c69b7dd4f902bec55e187776ecf
pbayona <eddybayonne@gmail.com>
2021-01-27 21:13:49 +0000

    Open API detailed

--- src/main/java/com/yebisu/medusa/config/OpenApiConfiguration.java
+++ src/main/java/com/yebisu/medusa/config/OpenApiConfiguration.java
@@ -1,14 +1,19 @@
-//package com.yebisu.medusa.config;
-//
-//import io.swagger.v3.oas.models.OpenAPI;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class OpenApiConfiguration {
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI();
-//    }
-//
-//}
+package com.yebisu.medusa.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                .title("Medusa Proxy API")
+                .description("Medusa proxy is the component which stores and manages the vehicle as well as stablishing the\n" +
+                        "communication between web app (clients) and ist-legacy-back-end")
+                .termsOfService("https://github.com/istyebisu"));
+    }
+}
