5218b11d6e5de452763ee37a0c417f03e9d6d8e5
pbayona <eddybayonne@gmail.com>
2021-02-01 21:54:38 +0000

    removed all old implementation of vehcileConfiguration which has to be in configServer

--- src/main/java/com/yebisu/medusa/controller/RosMessageController.java
+++ src/main/java/com/yebisu/medusa/controller/RosMessageController.java
@@ -1,18 +1,15 @@
 package com.yebisu.medusa.controller;
 
-import com.yebisu.medusa.controller.dto.VehicleStateInputDTO;
 import com.yebisu.medusa.service.VehicleService;
 import com.yebisu.medusa.service.dto.VehicleState;
 import com.yebisu.medusa.util.API;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping(API.VEHICLE_BASE_API)
@@ -22,11 +19,10 @@ public class RosMessageController {
 
     private final VehicleService vehicleService;
 
-
-    @PostMapping(value = "/state")
-    public Mono<VehicleState> getState(@RequestBody @Valid final VehicleStateInputDTO vehicleStateInput) {
-        log.debug("GET: Vehicle state: {}", vehicleStateInput);
-        return Mono.just(vehicleService.getState(vehicleStateInput.getVehicleIP()))
+    @PostMapping(value = "/{id}/state")
+    public Mono<VehicleState> getState(@PathVariable final String id) {
+        log.debug("GET: Vehicle state: {}", id);
+        return Mono.just(vehicleService.getState(id))
                 .log();
     }
 
--- src/main/java/com/yebisu/medusa/service/VehicleService.java
+++ src/main/java/com/yebisu/medusa/service/VehicleService.java
@@ -1,22 +1,7 @@
 package com.yebisu.medusa.service;
 
-import com.yebisu.medusa.domain.VehicleConfiguration;
 import com.yebisu.medusa.service.dto.VehicleState;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 public interface VehicleService {
-    Mono<VehicleConfiguration> create(VehicleConfiguration vehicleConfiguration);
-
-    Mono<VehicleConfiguration> findById(String id);
-
     VehicleState getState(String ip);
-
-    Flux<VehicleConfiguration> findAll();
-
-    Mono<VehicleConfiguration> update(String id, VehicleConfiguration vehicleConfiguration);
-
-    Mono<Void> deleteAll();
-
-    Mono<Void> deleteById(String id);
 }
