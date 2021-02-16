66310ca0afe5b3b468bd0ea6b13eb480049e1c2a
Pascoal Bayonne <eddybayonne@gmail.com>
2021-02-16 21:19:57 +0000

    reflecting the changes of the vehicle api which has changed the endpoint

--- src/main/java/com/yebisu/medusa/config/OpenApiConfiguration.java
+++ src/main/java/com/yebisu/medusa/config/OpenApiConfiguration.java
@@ -9,11 +9,12 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
+        Info info = new Info()
                 .title("Medusa Proxy API")
                 .description("Medusa proxy is the component which stores and manages the vehicle as well as stablishing the\n" +
                         "communication between web app (clients) and ist-legacy-back-end")
-                .termsOfService("https://github.com/istyebisu"));
+                .termsOfService("https://github.com/istyebisu");
+        return new OpenAPI()
+                .info(info);
     }
 }
